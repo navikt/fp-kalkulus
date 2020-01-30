d4ee539953c184f9ed3e2f141de27282ee06e7bb
Ole Jørgen Rishoff <ole.jorgen.rishoff@nav.no>
2020-01-30 14:15:55 +0100

    Opprett kontraktmodul og laget kontratk for Inntekter

---
+++ kontrakt/src/main/java/no/nav/folketrygdloven/v1/kodeverk/Kodeverk.java
@@ -0,0 +1,63 @@
+package no.nav.folketrygdloven.v1.kodeverk;
+
+import java.util.Objects;
+
+import javax.validation.constraints.AssertTrue;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(value = Include.NON_ABSENT, content = Include.NON_EMPTY)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "kodeverk", defaultImpl = Void.class)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = InntektskildeType.class, name = InntektskildeType.KODEVERK),
+})
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
+public abstract class Kodeverk {
+
+    @JacksonInject
+    private KodeValidator kodeValidator;
+
+    protected Kodeverk() {
+        // default ctor
+    }
+
+    /**
+     * Kode for angitt kodeverk. Gyldige verdier og validering er per kodeverk klasse.
+     */
+    public abstract String getKode();
+
+    /**
+     * Kodeverk - må matche kodeverk property generert for klassen.
+     */
+    public abstract String getKodeverk();
+
+    @AssertTrue
+    private boolean isOk() {
+        return kodeValidator == null || kodeValidator.valider(getKodeverk(), getKode()) == null;
+    }
+    @Override
+    public String toString() {
+        return getKodeverk() +"<"+ getKode() + ">";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==this)return true;
+        if(obj==null || obj.getClass() != this.getClass()) return false;
+        var other = getClass().cast(obj);
+        return Objects.equals(this.getKode(), other.getKode())
+                && Objects.equals(this.getKodeverk(), other.getKodeverk());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getKode(), getKodeverk());
+    }
+}
