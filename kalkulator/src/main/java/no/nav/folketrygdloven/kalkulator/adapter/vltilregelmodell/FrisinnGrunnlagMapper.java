3309817b41a3491e72cfb7dfa0f7483b2dcf88f8
Petter Ekern <43809623+pekern@users.noreply.github.com>
2020-05-12 15:40:26 +0200

    FIX: Fjerner input fra søker på nyoppstartet eller ikke (#725)

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/adapter/vltilregelmodell/FrisinnGrunnlagMapper.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/adapter/vltilregelmodell/FrisinnGrunnlagMapper.java
@@ -7,8 +7,6 @@ import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.ytelse.frisinn.Fris
 import no.nav.folketrygdloven.kalkulator.FagsakYtelseTypeRef;
 import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagDto;
-import no.nav.folketrygdloven.kalkulator.modell.iay.OppgittFrilansDto;
-import no.nav.folketrygdloven.kalkulator.modell.iay.OppgittOpptjeningDto;
 
 @ApplicationScoped
 @FagsakYtelseTypeRef("FRISINN")
@@ -16,12 +14,11 @@ public class FrisinnGrunnlagMapper implements YtelsesspesifikkRegelMapper {
 
     @Override
     public YtelsesSpesifiktGrunnlag map(BeregningsgrunnlagDto beregningsgrunnlagDto, BeregningsgrunnlagInput input) {
-        Boolean erNyoppstartetFrilans = input.getIayGrunnlag().getOppgittOpptjening().flatMap(OppgittOpptjeningDto::getFrilans).map(OppgittFrilansDto::getErNyoppstartet).orElse(false);
         if (!(input.getYtelsespesifiktGrunnlag() instanceof no.nav.folketrygdloven.kalkulator.KLASSER_MED_AVHENGIGHETER.FrisinnGrunnlag)) {
             throw new IllegalStateException("Mangler frisinngrunnlag for frisinnberegning");
         }
         no.nav.folketrygdloven.kalkulator.KLASSER_MED_AVHENGIGHETER.FrisinnGrunnlag frisinnGrunnlag = input.getYtelsespesifiktGrunnlag();
-        return new FrisinnGrunnlag(erNyoppstartetFrilans, frisinnGrunnlag.getSøkerYtelseForFrilans());
+        return new FrisinnGrunnlag(frisinnGrunnlag.getSøkerYtelseForFrilans());
     }
 
 }
