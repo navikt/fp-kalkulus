9030e69769b477f5f48553d1c8fe218d51f66f68
Petter Ekern <43809623+pekern@users.noreply.github.com>
2020-05-14 09:03:31 +0200

    Revert "TSF-3314: Returnerer ventepunkt dersom FRISINN og ATFL i samme org (#733)" (#739)
    This reverts commit 29cb9279fd51c6e2ca3385c473d965180cd01a14.

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/ytelse/frisinn/AksjonspunktUtlederFaktaOmBeregningFRISINN.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/ytelse/frisinn/AksjonspunktUtlederFaktaOmBeregningFRISINN.java
@@ -1,18 +1,10 @@
 package no.nav.folketrygdloven.kalkulator.ytelse.frisinn;
 
-import static no.nav.folketrygdloven.kalkulus.felles.tid.AbstractIntervall.TIDENES_ENDE;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import no.nav.folketrygdloven.kalkulator.AksjonspunktUtlederFaktaOmBeregning;
 import no.nav.folketrygdloven.kalkulator.FagsakYtelseTypeRef;
@@ -20,16 +12,12 @@ import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.kontrollerfakta.FaktaOmBeregningTilfelleTjeneste;
 import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagDto;
 import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagGrunnlagDto;
-import no.nav.folketrygdloven.kalkulator.output.BeregningAksjonspunktResultat;
-import no.nav.folketrygdloven.kalkulator.output.BeregningVenteårsak;
 import no.nav.folketrygdloven.kalkulator.output.FaktaOmBeregningAksjonspunktResultat;
-import no.nav.folketrygdloven.kalkulus.felles.kodeverk.domene.BeregningAksjonspunktDefinisjon;
 import no.nav.folketrygdloven.kalkulus.felles.kodeverk.domene.FaktaOmBeregningTilfelle;
 
 @ApplicationScoped
 @FagsakYtelseTypeRef("FRISINN")
 public class AksjonspunktUtlederFaktaOmBeregningFRISINN extends AksjonspunktUtlederFaktaOmBeregning {
-    private static final Logger LOG = LoggerFactory.getLogger(AksjonspunktUtlederFaktaOmBeregningFRISINN.class);
 
     public AksjonspunktUtlederFaktaOmBeregningFRISINN() {
         // CDI
@@ -48,11 +36,7 @@ public class AksjonspunktUtlederFaktaOmBeregningFRISINN extends AksjonspunktUtle
         Objects.requireNonNull(beregningsgrunnlag, "beregningsgrunnlag");
         List<FaktaOmBeregningTilfelle> faktaOmBeregningTilfeller = faktaOmBeregningTilfelleTjeneste.finnTilfellerForFellesAksjonspunkt(input, beregningsgrunnlagGrunnlag);
         if (faktaOmBeregningTilfeller.contains(FaktaOmBeregningTilfelle.VURDER_AT_OG_FL_I_SAMME_ORGANISASJON)) {
-            LOG.info("Kan ikke behandle FRISINN-ytelse for AT og FL i samme organisajon. Setter på vent. BehandlingID: " + input.getBehandlingReferanse().getBehandlingId());
-            BeregningAksjonspunktResultat ventepunkt = BeregningAksjonspunktResultat.opprettMedFristFor(BeregningAksjonspunktDefinisjon.FRISINN_ATFL_SAMME_ORG,
-                    BeregningVenteårsak.FRISINN_ATFL_SAMME_ORG,
-                    LocalDateTime.of(TIDENES_ENDE, LocalTime.MIDNIGHT));
-            return new FaktaOmBeregningAksjonspunktResultat(Collections.singletonList(ventepunkt), Collections.emptyList());
+            throw new IllegalStateException("Kan ikke behandle FRISINN-ytelse for AT og FL i samme organisajon. BehandlingID: " + input.getBehandlingReferanse().getBehandlingId());
         }
         return FaktaOmBeregningAksjonspunktResultat.INGEN_AKSJONSPUNKTER;
     }
