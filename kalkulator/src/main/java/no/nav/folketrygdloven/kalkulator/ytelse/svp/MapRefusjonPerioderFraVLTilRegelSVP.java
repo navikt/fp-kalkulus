0ac355f733dd0285cbc9af48bcc809b113e241e7
Petter Ekern <43809623+pekern@users.noreply.github.com>
2021-04-26 20:44:05 +0200

    TFP-4309: Sjekker om utledet startdato er før opphør av refusjon fra inntektsmeldingen (#1890)

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/ytelse/svp/MapRefusjonPerioderFraVLTilRegelSVP.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/ytelse/svp/MapRefusjonPerioderFraVLTilRegelSVP.java
@@ -1,5 +1,7 @@
 package no.nav.folketrygdloven.kalkulator.ytelse.svp;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -12,16 +14,48 @@ import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.Periode;
 import no.nav.folketrygdloven.kalkulator.FagsakYtelseTypeRef;
 import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.input.UtbetalingsgradGrunnlag;
+import no.nav.folketrygdloven.kalkulator.input.YtelsespesifiktGrunnlag;
+import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagDto;
 import no.nav.folketrygdloven.kalkulator.modell.iay.InntektsmeldingDto;
 import no.nav.folketrygdloven.kalkulator.modell.svp.PeriodeMedUtbetalingsgradDto;
 import no.nav.folketrygdloven.kalkulator.modell.svp.UtbetalingsgradPrAktivitetDto;
 import no.nav.folketrygdloven.kalkulator.modell.typer.Arbeidsgiver;
 import no.nav.folketrygdloven.kalkulator.modell.typer.InternArbeidsforholdRefDto;
+import no.nav.folketrygdloven.kalkulator.tid.Intervall;
 import no.nav.folketrygdloven.kalkulator.ytelse.utbgradytelse.MapRefusjonPerioderFraVLTilRegelUtbgrad;
 
 @FagsakYtelseTypeRef("SVP")
 @ApplicationScoped
 public class MapRefusjonPerioderFraVLTilRegelSVP extends MapRefusjonPerioderFraVLTilRegelUtbgrad {
+
+    /** Finner gyldige perioder for refusjon basert på perioder med utbetalingsgrad og inntektsmeldingsdata
+     *
+     *
+     * @param startdatoPermisjon Startdato for permisjonen for ytelse søkt for
+     * @param ytelsespesifiktGrunnlag Ytelsesspesifikt grunnlag
+     * @param inntektsmelding                      inntektsmelding
+     * @param beregningsgrunnlag
+     * @return Gyldige perioder for refusjon
+     */
+    @Override
+    protected List<Intervall> finnGyldigeRefusjonPerioder(LocalDate startdatoPermisjon,
+                                                          YtelsespesifiktGrunnlag ytelsespesifiktGrunnlag,
+                                                          InntektsmeldingDto inntektsmelding, BeregningsgrunnlagDto beregningsgrunnlag) {
+        if (inntektsmelding.getRefusjonOpphører() != null && inntektsmelding.getRefusjonOpphører().isBefore(startdatoPermisjon)) {
+            // Refusjon opphører før det utledede startpunktet, blir aldri refusjon
+            return Collections.emptyList();
+        }
+        if (ytelsespesifiktGrunnlag instanceof UtbetalingsgradGrunnlag) {
+            var utbetalingsgradGrunnlag = (UtbetalingsgradGrunnlag) ytelsespesifiktGrunnlag;
+            var perioderMedUtbetaling = utbetalingsgradGrunnlag.finnUtbetalingsgraderForArbeid(inntektsmelding.getArbeidsgiver(), inntektsmelding.getArbeidsforholdRef())
+                    .stream()
+                    .filter(p -> p.getUtbetalingsgrad() != null && p.getUtbetalingsgrad().compareTo(BigDecimal.ZERO) > 0)
+                    .map(PeriodeMedUtbetalingsgradDto::getPeriode)
+                    .collect(Collectors.toList());
+            return perioderMedUtbetaling;
+        }
+        return List.of();
+    }
 
     @Override
     protected List<Gradering> mapUtbetalingsgrader(InntektsmeldingDto im, BeregningsgrunnlagInput input) {
@@ -43,8 +77,4 @@ public class MapRefusjonPerioderFraVLTilRegelSVP extends MapRefusjonPerioderFraV
         boolean gjelderSammeAG = Objects.equals(im.getArbeidsgiver(), utbAG);
         return gjelderSammeAG && im.getArbeidsforholdRef().gjelderFor(utbRef);
     }
-
-    // Fordi annotasjoner propageres til subklasser
-    // Burde vurdere å gjere om til interface
-
 }
