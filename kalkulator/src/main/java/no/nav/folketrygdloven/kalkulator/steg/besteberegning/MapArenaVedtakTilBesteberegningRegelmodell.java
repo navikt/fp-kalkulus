d03c23e02e5d4bdc8a43d2172edabe4778fec5ea
Petter Ekern <43809623+pekern@users.noreply.github.com>
2021-08-16 11:11:37 +0200

    TFP-4532: Logger saker der meldekort starter før vedtak (#2123)

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/besteberegning/MapArenaVedtakTilBesteberegningRegelmodell.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/besteberegning/MapArenaVedtakTilBesteberegningRegelmodell.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.Periode;
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.grunnlag.inntekt.Inntektskilde;
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.grunnlag.inntekt.Periodeinntekt;
@@ -17,6 +20,7 @@ import no.nav.folketrygdloven.kalkulator.modell.typer.Beløp;
  * For arenaytelser mapper vi alle meldekort om til inntektsperioder og bryr oss ikke om vedtakene
  */
 public class MapArenaVedtakTilBesteberegningRegelmodell {
+    private static final Logger LOG = LoggerFactory.getLogger(MapArenaVedtakTilBesteberegningRegelmodell.class);
 
     private MapArenaVedtakTilBesteberegningRegelmodell() {
         // Skjuler default
@@ -25,6 +29,7 @@ public class MapArenaVedtakTilBesteberegningRegelmodell {
     public static List<Periodeinntekt> lagInntektFraArenaYtelser(YtelseFilterDto ytelseFilter) {
         Collection<YtelseDto> arenaytelser = ytelseFilter.filter(ytelse -> ytelse.getRelatertYtelseType().erArenaytelse())
                 .getFiltrertYtelser();
+        loggMeldekortSomStarterFørVedtak(arenaytelser);
         List<YtelseAnvistDto> alleMeldekortFraArena = arenaytelser.stream()
                 .map(YtelseDto::getYtelseAnvist)
                 .flatMap(Collection::stream)
@@ -41,4 +46,19 @@ public class MapArenaVedtakTilBesteberegningRegelmodell {
                 .medPeriode(Periode.of(mk.getAnvistFOM(), mk.getAnvistTOM()))
                 .build();
     }
+
+    // Logger slike tilfeller for å se hvor mange av de vi har før vi bestemmer oss for å gjøre endringer
+    private static void loggMeldekortSomStarterFørVedtak(Collection<YtelseDto> arenaytelser) {
+        arenaytelser.forEach(vedtak -> {
+            List<YtelseAnvistDto> meldekortSomStarterFørVedtak = vedtak.getYtelseAnvist().stream()
+                    .filter(meldekort -> vedtak.getPeriode().getFomDato().isBefore(meldekort.getAnvistFOM()))
+                    .collect(Collectors.toList());
+            meldekortSomStarterFørVedtak.forEach(mk -> {
+                String msg = String.format("FT-684563: Meldekort starter før vedtak, meldekort har startdato %s mens tilhørende vedtak har startdato %s", mk.getAnvistFOM(), vedtak.getPeriode().getFomDato());
+                LOG.info(msg);
+            });
+        });
+
+    }
+
 }
