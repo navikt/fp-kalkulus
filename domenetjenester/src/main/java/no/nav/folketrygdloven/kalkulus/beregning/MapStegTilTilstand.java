df5bcd1d237e17a08665b6000aa8df79fc38158d
Jan Olav Eide <80393418+jan-olaveide@users.noreply.github.com>
2021-08-18 20:05:56 +0200

    simplify map (#2125)

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/beregning/MapStegTilTilstand.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/beregning/MapStegTilTilstand.java
@@ -1,6 +1,25 @@
 package no.nav.folketrygdloven.kalkulus.beregning;
 
-import java.util.HashMap;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.FASTSETT_STP_BER;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.FAST_BERGRUNN;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.FORDEL_BERGRUNN;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.FORS_BERGRUNN;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.FORS_BESTEBEREGNING;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.KOFAKBER;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningSteg.VURDER_REF_BERGRUNN;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.BESTEBEREGNET;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.FASTSATT;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.FASTSATT_BEREGNINGSAKTIVITETER;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.FASTSATT_INN;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.FORESLÅTT;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.FORESLÅTT_UT;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.KOFAKBER_UT;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.OPPDATERT_MED_ANDELER;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.OPPDATERT_MED_REFUSJON_OG_GRADERING;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.OPPRETTET;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.VURDERT_REFUSJON;
+import static no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand.VURDERT_REFUSJON_UT;
+
 import java.util.Map;
 import java.util.Optional;
 
@@ -9,34 +28,32 @@ import no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand;
 
 public class MapStegTilTilstand {
 
-    private static Map<BeregningSteg, BeregningsgrunnlagTilstand> mapStegTilstand = new HashMap<>();
-    private static Map<BeregningSteg, BeregningsgrunnlagTilstand> mapStegUtTilstand = new HashMap<>();
+    private static final Map<BeregningSteg, BeregningsgrunnlagTilstand> MAP_STEG_TILSTAND = Map.of(
+            FASTSETT_STP_BER, OPPRETTET,
+            KOFAKBER, OPPDATERT_MED_ANDELER,
+            FORS_BESTEBEREGNING, BESTEBEREGNET,
+            FORS_BERGRUNN, FORESLÅTT,
+            VURDER_REF_BERGRUNN, VURDERT_REFUSJON,
+            FORDEL_BERGRUNN, OPPDATERT_MED_REFUSJON_OG_GRADERING,
+            FAST_BERGRUNN, FASTSATT);
 
-    static {
-        mapStegTilstand.put(BeregningSteg.FASTSETT_STP_BER, BeregningsgrunnlagTilstand.OPPRETTET);
-        mapStegTilstand.put(BeregningSteg.KOFAKBER, BeregningsgrunnlagTilstand.OPPDATERT_MED_ANDELER);
-        mapStegTilstand.put(BeregningSteg.FORS_BESTEBEREGNING, BeregningsgrunnlagTilstand.BESTEBEREGNET);
-        mapStegTilstand.put(BeregningSteg.FORS_BERGRUNN, BeregningsgrunnlagTilstand.FORESLÅTT);
-        mapStegTilstand.put(BeregningSteg.VURDER_REF_BERGRUNN, BeregningsgrunnlagTilstand.VURDERT_REFUSJON);
-        mapStegTilstand.put(BeregningSteg.FORDEL_BERGRUNN, BeregningsgrunnlagTilstand.OPPDATERT_MED_REFUSJON_OG_GRADERING);
-        mapStegTilstand.put(BeregningSteg.FAST_BERGRUNN, BeregningsgrunnlagTilstand.FASTSATT);
+    private static final Map<BeregningSteg, BeregningsgrunnlagTilstand> MAP_STEG_UT_TILSTAND = Map.of(
+            FASTSETT_STP_BER, FASTSATT_BEREGNINGSAKTIVITETER,
+            KOFAKBER, KOFAKBER_UT,
+            FORS_BERGRUNN, FORESLÅTT_UT,
+            VURDER_REF_BERGRUNN, VURDERT_REFUSJON_UT,
+            FORDEL_BERGRUNN, FASTSATT_INN);
 
-        mapStegUtTilstand.put(BeregningSteg.FASTSETT_STP_BER, BeregningsgrunnlagTilstand.FASTSATT_BEREGNINGSAKTIVITETER);
-        mapStegUtTilstand.put(BeregningSteg.KOFAKBER, BeregningsgrunnlagTilstand.KOFAKBER_UT);
-        mapStegUtTilstand.put(BeregningSteg.FORS_BERGRUNN, BeregningsgrunnlagTilstand.FORESLÅTT_UT);
-        mapStegUtTilstand.put(BeregningSteg.VURDER_REF_BERGRUNN, BeregningsgrunnlagTilstand.VURDERT_REFUSJON_UT);
-        mapStegUtTilstand.put(BeregningSteg.FORDEL_BERGRUNN, BeregningsgrunnlagTilstand.FASTSATT_INN);
-    }
 
     public static BeregningsgrunnlagTilstand mapTilStegTilstand(BeregningSteg kode) {
-        if (mapStegTilstand.containsKey(kode)) {
-            return mapStegTilstand.get(kode);
+        if (MAP_STEG_TILSTAND.containsKey(kode)) {
+            return MAP_STEG_TILSTAND.get(kode);
         }
         throw new IllegalStateException("Finner ikke tilstand for steg " + kode.getKode());
     }
 
     public static Optional<BeregningsgrunnlagTilstand> mapTilStegUtTilstand(BeregningSteg kode) {
-        return Optional.ofNullable(mapStegUtTilstand.get(kode));
+        return Optional.ofNullable(MAP_STEG_UT_TILSTAND.get(kode));
     }
 
 
