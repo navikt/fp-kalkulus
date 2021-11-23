2dd858aea66e39f5eb195698ea850378d118e2fe
Espen Velsvik <espenjv@users.noreply.github.com>
2021-11-23 09:23:33 +0100

    TSF-2202 Gjenoppretter grunnlag som er feilaktiv deaktivert (#2480)

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_39__gjenopprett_grunnlag.sql
@@ -0,0 +1,465 @@
+/* Saksnummer ACRD8 */
+update kalkulator_input
+set aktiv = true
+where id = 1791941;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 4998380;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909392075,
+             909392076,
+             909392077,
+             909392152,
+             909392153
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             676847629,
+             676847630,
+             676847631,
+             676847632,
+             676847633,
+             676847634,
+             676847635,
+             676847636,
+             676847637,
+             676847638,
+             676847639,
+             676847640,
+             676847641,
+             676847642,
+             676847643,
+             676847644,
+             676847645,
+             676847646,
+             676847647,
+             676847648,
+             676847649,
+             676847650,
+             676847751,
+             676847753,
+             676847754,
+             676847755,
+             676847756,
+             676847757,
+             676847758,
+             676847759,
+             676847760,
+             676847761,
+             676847762,
+             676847763,
+             676847764,
+             676847765,
+             676847766,
+             676847767,
+             676847768,
+             676847769,
+             676847770,
+             676847771,
+             676847772,
+             676847773,
+             676847774,
+             676847775,
+             676847776,
+             676847752,
+             676846339
+    );
+
+
+/* Saksnummer ACPV2 */
+update kalkulator_input
+set aktiv = true
+where id = 1796884;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 5038461;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909403821,
+             909403822,
+             909403823,
+             909403826,
+             909403827
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             677168226,
+             677168512,
+             677168514,
+             677168515,
+             677168516,
+             677168517,
+             677168518,
+             677168519,
+             677168520,
+             677168521,
+             677168522,
+             677168523,
+             677168524,
+             677168525,
+             677168526,
+             677168527,
+             677168528,
+             677168529,
+             677168530,
+             677168531,
+             677168532,
+             677168533,
+             677168534,
+             677168535,
+             677168536
+    );
+
+/* Saksnummer ACN4G */
+update kalkulator_input
+set aktiv = true
+where id = 1795822;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 5032270;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909400803,
+             909400804,
+             909400805,
+             909400806,
+             909400807
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             677109774,
+             677109775,
+             677109776,
+             677109777,
+             677109778,
+             677109779,
+             677109780,
+             677109781,
+             677109782,
+             677109783,
+             677109784,
+             677109785,
+             677109786,
+             677109787,
+             677109788,
+             677109789,
+             677109790,
+             677109791,
+             677109792,
+             677109793,
+             677109794,
+             677109795,
+             677109796,
+             677109797,
+             677109798,
+             677109799,
+             677109800,
+             677109801,
+             677109802,
+             677109803,
+             677109804,
+             677109805
+    );
+
+/* Saksnummer ACLJi */
+update kalkulator_input
+set aktiv = true
+where id = 1783457;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 4959220;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909365395,
+             909365396,
+             909365397,
+             909365398,
+             909365399
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             676448094,
+             676448095,
+             676448096,
+             676448097,
+             676448098
+    );
+
+/* Saksnummer ACLNo */
+update kalkulator_input
+set aktiv = true
+where id = 1794380;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 5023959;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909397072,
+             909397073,
+             909397074,
+             909397075,
+             909397076
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             677026264,
+             677026265,
+             677026266,
+             677026267,
+             677026268,
+             677026269,
+             677026270,
+             677026271,
+             677026272,
+             677026273,
+             677026274,
+             677026275,
+             677026276,
+             677026277,
+             677026278,
+             677026279,
+             677026280,
+             677026281,
+             677026282,
+             677026283
+    );
+
+/* Saksnummer AEGMS */
+update kalkulator_input
+set aktiv = true
+where id = 2147782;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 2147782;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909404749,
+             909404750,
+             909404801,
+             909404802,
+             909404803
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             677186893,
+             677186894,
+             677186895,
+             677186896,
+             677186897,
+             677186898,
+             677186899,
+             677186900,
+             677186901,
+             677186902,
+             677186903,
+             677186904,
+             677186905,
+             677186906
+    );
+
+/* Saksnummer ABK2W */
+update kalkulator_input
+set aktiv = true
+where id = 1786883;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 4979636;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909374784,
+             909374785,
+             909374786,
+             909374717,
+             909374718
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             676640021,
+             676640022,
+             676639914,
+             676639915,
+             676639916,
+             676639917,
+             676639918,
+             676639919,
+             676639920,
+             676639921,
+             676639922,
+             676639923,
+             676639925,
+             676639926,
+             676639927,
+             676639928,
+             676639929,
+             676639930,
+             676639931,
+             676639924
+    );
+
+/* Saksnummer ADTZi */
+update kalkulator_input
+set aktiv = true
+where id = 1816274;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 5148020;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909467582,
+             909467583,
+             909467584,
+             909467585,
+             909467586
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             678033662,
+             678033663,
+             678033664,
+             678033665,
+             678033666,
+             678033667,
+             678033668,
+             678033669,
+             678033670,
+             678033671,
+             678033672,
+             678033673,
+             678033674,
+             678033675,
+             678033676,
+             678033677,
+             678033678,
+             678033679,
+             678033680,
+             678033681,
+             678033682,
+             678033683,
+             678033684,
+             678033685,
+             678033686,
+             678033687,
+             678033688,
+             678033689,
+             678033690,
+             678033691,
+             678033692,
+             678033693,
+             678033694,
+             678033695,
+             678033696,
+             678033697,
+             678033698,
+             678033699,
+             678033700,
+             678033701,
+             678033702,
+             678033703,
+             678033704,
+             678033705,
+             678033706,
+             678033707,
+             678033708,
+             678033709,
+             678033710,
+             678033711,
+             678033712,
+             678033713,
+             678033714,
+             678033715,
+             678033716,
+             678033717,
+             678033718,
+             678033719,
+             678033720,
+             678033721,
+             678033722,
+             678033723
+    );
+
+
+/* Saksnummer ACNCS */
+update kalkulator_input
+set aktiv = true
+where id = 1795508;
+
+update gr_beregningsgrunnlag
+set aktiv = true
+where id = 2146091;
+
+update regel_sporing_grunnlag
+set aktiv = true
+where id in (
+             909399907,
+             909399908,
+             909399909,
+             909399910,
+             909399911
+    );
+
+update regel_sporing_periode
+set aktiv = true
+where id in (
+             677090268,
+             677090269,
+             677090270,
+             677090271,
+             677090272,
+             677090273,
+             677090274,
+             677090275,
+             677090276,
+             677090277,
+             677090278,
+             677090279,
+             677090280,
+             677090281,
+             677090282,
+             677090283,
+             677090284,
+             677090285,
+             677090286,
+             677090287
+    );
+
