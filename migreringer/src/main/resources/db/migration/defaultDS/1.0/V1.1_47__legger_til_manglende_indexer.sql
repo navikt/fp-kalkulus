9e0a8e81aa7454f14318212c43a909d9dca62257
Marius Glittum <marius.glittum@nav.no>
2022-01-04 14:39:43 +0100

    Legger til flere manglende indexer

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_47__legger_til_manglende_indexer.sql
@@ -0,0 +1,3 @@
+create index IDX_AVKLARINGSBEHOV_10 on AVKLARINGSBEHOV (kobling_id);
+create index IDX_AVKLARINGSBEHOV_11 on AVKLARINGSBEHOV (avklaringsbehov_def);
+create index IDX_AVKLARINGSBEHOV_11 on AVKLARINGSBEHOV (avklaringsbehov_status);
