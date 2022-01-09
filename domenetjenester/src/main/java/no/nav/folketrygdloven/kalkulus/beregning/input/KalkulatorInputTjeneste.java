56090f9682f7e60f90eb19dedc6c07344b474895
Frode Carlsen <frode.odde.carlsen@nav.no>
2022-01-09 13:15:19 +0000

    Revert til uten hibernate-types eller logging av feil input

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/beregning/input/KalkulatorInputTjeneste.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/beregning/input/KalkulatorInputTjeneste.java
@@ -1,7 +1,5 @@
 package no.nav.folketrygdloven.kalkulus.beregning.input;
 
-import java.nio.charset.Charset;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -44,8 +42,7 @@ public class KalkulatorInputTjeneste {
     private KoblingTjeneste koblingTjeneste;
 
     @Inject
-    public KalkulatorInputTjeneste(BeregningsgrunnlagRepository beregningsgrunnlagRepository,
-            KoblingTjeneste koblingTjeneste) {
+    public KalkulatorInputTjeneste(BeregningsgrunnlagRepository beregningsgrunnlagRepository, KoblingTjeneste koblingTjeneste) {
         this.beregningsgrunnlagRepository = beregningsgrunnlagRepository;
         this.koblingTjeneste = koblingTjeneste;
     }
@@ -54,8 +51,7 @@ public class KalkulatorInputTjeneste {
         // CDI-runner
     }
 
-    public Map<Long, KalkulatorInputDto> hentOgLagre(Map<UUID, KalkulatorInputDto> inputPrReferanse,
-            Set<Long> koblingIder) {
+    public Map<Long, KalkulatorInputDto> hentOgLagre(Map<UUID, KalkulatorInputDto> inputPrReferanse, Set<Long> koblingIder) {
         if (inputPrReferanse != null && !inputPrReferanse.isEmpty()) {
             // kalkulatorinput oppdateres
             lagreKalkulatorInput(inputPrReferanse);
@@ -64,27 +60,25 @@ public class KalkulatorInputTjeneste {
     }
 
     public Map<Long, KalkulatorInputDto> hentOgLagre(Map<UUID, KalkulatorInputDto> inputPrReferanse,
-            Map<UUID, YtelsespesifiktGrunnlagDto> ytelsespesifiktGrunnlagPrKoblingReferanse,
-            Set<Long> koblingIder) throws UgyldigInputException {
+                                                     Map<UUID, YtelsespesifiktGrunnlagDto> ytelsespesifiktGrunnlagPrKoblingReferanse,
+                                                     Set<Long> koblingIder) throws UgyldigInputException {
         if (inputPrReferanse != null && !inputPrReferanse.isEmpty()) {
             // kalkulatorinput oppdateres
             lagreKalkulatorInput(inputPrReferanse);
-        } else if (ytelsespesifiktGrunnlagPrKoblingReferanse != null
-                && !ytelsespesifiktGrunnlagPrKoblingReferanse.isEmpty()) {
+        } else if (ytelsespesifiktGrunnlagPrKoblingReferanse != null && !ytelsespesifiktGrunnlagPrKoblingReferanse.isEmpty()) {
             oppdaterYtelsesspesifiktGrunnlag(ytelsespesifiktGrunnlagPrKoblingReferanse);
         }
         return hentForKoblinger(koblingIder);
     }
 
+
+
     public Map<Long, KalkulatorInputDto> hentForKoblinger(Collection<Long> koblingId) throws UgyldigInputException {
         var kalkulatorInputEntitetListe = beregningsgrunnlagRepository.hentHvisEksistererKalkulatorInput(koblingId);
-        List<Long> koblingUtenInput = koblingId.stream()
-                .filter(id -> kalkulatorInputEntitetListe.stream().map(KalkulatorInputEntitet::getKoblingId)
-                        .noneMatch(k -> k.equals(id)))
+        List<Long> koblingUtenInput = koblingId.stream().filter(id -> kalkulatorInputEntitetListe.stream().map(KalkulatorInputEntitet::getKoblingId).noneMatch(k -> k.equals(id)))
                 .collect(Collectors.toList());
         if (!koblingUtenInput.isEmpty()) {
-            throw new TekniskException("FT-KALKULUS-INPUT-1000000",
-                    String.format("Kalkulus finner ikke kalkulator input for koblingId: %s", koblingUtenInput));
+            throw new TekniskException("FT-KALKULUS-INPUT-1000000", String.format("Kalkulus finner ikke kalkulator input for koblingId: %s", koblingUtenInput));
         }
         Map<Long, KalkulatorInputDto> inputMap = new HashMap<>();
 
@@ -102,17 +96,12 @@ public class KalkulatorInputTjeneste {
         try {
             input = READER.forType(KalkulatorInputDto.class).readValue(json);
         } catch (JsonProcessingException e) {
-            var encoded = Base64.getEncoder().encodeToString(json.getBytes(Charset.forName("UTF8")));
-            System.out.println(encoded);
-            throw new TekniskException("FT-KALKULUS-INPUT-1000002",
-                    String.format("Kalkulus klarte ikke lese opp input for koblingId %s med følgende feilmelding %s",
-                            koblingId, e.getMessage()));
+            throw new TekniskException("FT-KALKULUS-INPUT-1000002", String.format("Kalkulus klarte ikke lese opp input for koblingId %s med følgende feilmelding %s", koblingId, e.getMessage()));
         }
 
         var violations = VALIDATOR.validate(input);
         if (!violations.isEmpty()) {
-            throw new UgyldigInputException("FT-KALKULUS-INPUT-1000004", "Ugyldig kalkulatorinput: "
-                    + violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList()));
+            throw new UgyldigInputException("FT-KALKULUS-INPUT-1000004", "Ugyldig kalkulatorinput: " + violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList()));
         }
         return input;
 
@@ -124,22 +113,19 @@ public class KalkulatorInputTjeneste {
             input = WRITER.writeValueAsString(kalkulatorInput);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
-            throw new TekniskException("FT-KALKULUS-INPUT-1000002",
-                    String.format("Kalkulus klarte ikke lagre ned input for koblingId: %s med følgende feilmelding %s",
-                            koblingId, e.getMessage()));
+            throw new TekniskException("FT-KALKULUS-INPUT-1000002", String.format("Kalkulus klarte ikke lagre ned input for koblingId: %s med følgende feilmelding %s", koblingId, e.getMessage()));
         }
         return beregningsgrunnlagRepository.lagreOgSjekkStatus(new KalkulatorInputEntitet(koblingId, input));
     }
 
     public void lagreKalkulatorInput(YtelseTyperKalkulusStøtterKontrakt ytelseTyperKalkulusStøtter,
-            Map<UUID, KalkulatorInputDto> kalkulatorInputPerKoblingReferanse) {
+                                     Map<UUID, KalkulatorInputDto> kalkulatorInputPerKoblingReferanse) {
         List<KoblingEntitet> koblinger = koblingTjeneste.hentKoblinger(kalkulatorInputPerKoblingReferanse.keySet()
                 .stream()
                 .map(KoblingReferanse::new)
                 .collect(Collectors.toList()), ytelseTyperKalkulusStøtter);
         kalkulatorInputPerKoblingReferanse.forEach((ref, input) -> {
-            Optional<KoblingEntitet> kobling = koblinger.stream()
-                    .filter(k -> k.getKoblingReferanse().getReferanse().equals(ref)).findFirst();
+            Optional<KoblingEntitet> kobling = koblinger.stream().filter(k -> k.getKoblingReferanse().getReferanse().equals(ref)).findFirst();
             kobling.ifPresent(k -> lagreKalkulatorInput(k.getId(), input));
         });
     }
@@ -150,25 +136,20 @@ public class KalkulatorInputTjeneste {
                 .map(KoblingReferanse::new)
                 .collect(Collectors.toList()));
         kalkulatorInputPerKoblingReferanse.forEach((ref, input) -> {
-            Optional<KoblingEntitet> kobling = koblinger.stream()
-                    .filter(k -> k.getKoblingReferanse().getReferanse().equals(ref)).findFirst();
+            Optional<KoblingEntitet> kobling = koblinger.stream().filter(k -> k.getKoblingReferanse().getReferanse().equals(ref)).findFirst();
             kobling.ifPresent(k -> lagreKalkulatorInput(k.getId(), input));
         });
     }
 
-    public void oppdaterYtelsesspesifiktGrunnlag(
-            Map<UUID, YtelsespesifiktGrunnlagDto> ytelsespesifiktGrunnlagPrKoblingReferanse) {
-        List<KoblingEntitet> koblinger = koblingTjeneste
-                .hentKoblinger(ytelsespesifiktGrunnlagPrKoblingReferanse.keySet()
-                        .stream()
-                        .map(KoblingReferanse::new)
-                        .collect(Collectors.toList()));
-        var resultatPrKobling = hentForKoblinger(
-                koblinger.stream().map(KoblingEntitet::getId).collect(Collectors.toList()));
+    public void oppdaterYtelsesspesifiktGrunnlag(Map<UUID, YtelsespesifiktGrunnlagDto> ytelsespesifiktGrunnlagPrKoblingReferanse) {
+        List<KoblingEntitet> koblinger = koblingTjeneste.hentKoblinger(ytelsespesifiktGrunnlagPrKoblingReferanse.keySet()
+                .stream()
+                .map(KoblingReferanse::new)
+                .collect(Collectors.toList()));
+        var resultatPrKobling = hentForKoblinger(koblinger.stream().map(KoblingEntitet::getId).collect(Collectors.toList()));
         resultatPrKobling.forEach((key, value) -> {
             var kobling = koblinger.stream().filter(k -> k.getId().equals(key)).findFirst().orElseThrow();
-            var ytelsespesifiktGrunnlag = ytelsespesifiktGrunnlagPrKoblingReferanse
-                    .get(kobling.getKoblingReferanse().getReferanse());
+            var ytelsespesifiktGrunnlag = ytelsespesifiktGrunnlagPrKoblingReferanse.get(kobling.getKoblingReferanse().getReferanse());
             var oppdatertInput = value.medYtelsespesifiktGrunnlag(ytelsespesifiktGrunnlag);
             lagreKalkulatorInput(key, oppdatertInput);
         });
