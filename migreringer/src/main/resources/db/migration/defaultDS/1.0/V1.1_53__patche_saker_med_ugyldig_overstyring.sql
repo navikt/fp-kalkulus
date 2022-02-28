290d5a038ba7c2b740e9d51aa63f2c3d1f8f0ac7
Espen Velsvik <espenjv@users.noreply.github.com>
2022-02-28 12:14:50 +0100

    Patcher saker som har fått ugyldige overstyringer (#2866)

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_53__patche_saker_med_ugyldig_overstyring.sql
@@ -0,0 +1,7 @@
+-- Saksnummer A4SPS
+update gr_beregningsgrunnlag set aktiv = false where id = 5723811;
+update gr_beregningsgrunnlag set aktiv = true where id = 4524217;
+
+-- Saksnummer ARU6W
+update gr_beregningsgrunnlag set aktiv = false where id = 5851546;
+update gr_beregningsgrunnlag set aktiv = true where id = 5408374;
