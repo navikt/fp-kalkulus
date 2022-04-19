6e77b8d0725a0e3339342d7b7af2f167e706e82c
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-04-19 11:17:03 +0200

    Endrer driftsrolle i env

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/forvaltning/DiagnostikkBeregningRestTjeneste.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/forvaltning/DiagnostikkBeregningRestTjeneste.java
@@ -48,7 +48,7 @@ public class DiagnostikkBeregningRestTjeneste {
     @Path("/sak")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Operation(description = "Henter en dump av info for debugging og analyse av en sak. Logger hvem som har hatt innsyn i sak", summary = ("Henter en dump av info for debugging og analyse av en sak"), tags = "diagnostikk")
-    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, resource = KalkulusBeskyttetRessursAttributt.k9_DRIFT)
+    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, property = "abac.attributt.drift")
     public Response dumpSak(@NotNull @FormParam("saksnummer") @Parameter(description = "saksnummer", allowEmptyValue = false, required = true, schema = @Schema(type = "string", maximum = "10")) @Valid SaksnummerDto saksnummerDto,
                             @NotNull @FormParam("begrunnelse") @Parameter(description = "begrunnelse", allowEmptyValue = false, required = true, schema = @Schema(type = "string", maximum = "2000")) @Valid KortTekst begrunnelse) {
 
