83d689c83be46fcd1e794a71a58ad1efc1ec5852
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-05-12 20:34:28 +0200

    Legger til saksnummer i abacattributter ved bruk av saksnummer-type

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/forvaltning/SaksnummerDto.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/forvaltning/SaksnummerDto.java
@@ -15,6 +15,7 @@ import jakarta.validation.constraints.Size;
 import no.nav.folketrygdloven.kalkulus.domene.entiteter.del_entiteter.Saksnummer;
 import no.nav.k9.felles.sikkerhet.abac.AbacDataAttributter;
 import no.nav.k9.felles.sikkerhet.abac.AbacDto;
+import no.nav.k9.felles.sikkerhet.abac.StandardAbacAttributtType;
 
 @JsonFormat(shape = Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY, isGetterVisibility = Visibility.NONE)
@@ -60,6 +61,7 @@ public class SaksnummerDto implements AbacDto {
     @Override
     public AbacDataAttributter abacAttributter() {
         final var abacDataAttributter = AbacDataAttributter.opprett();
+        abacDataAttributter.leggTil(StandardAbacAttributtType.SAKSNUMMER, getVerdi().getVerdi());
         return abacDataAttributter;
     }
 
