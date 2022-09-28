e1804e638ce6979e88acd23795feee8436fea894
Espen Velsvik <espenjv@users.noreply.github.com>
2022-09-28 21:04:18 +0200

    Legger til journalpostId i oppgitt opptjening (#3425)

--- kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/opptjening/v1/OppgittOpptjeningDto.java
+++ kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/opptjening/v1/OppgittOpptjeningDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
+import no.nav.folketrygdloven.kalkulus.felles.v1.JournalpostId;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -13,6 +14,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
 public class OppgittOpptjeningDto {
+
+    // Journalpost-id for sporing
+    @JsonProperty(value = "journalpostId")
+    @Valid
+    private JournalpostId journalpostId;
 
     @JsonProperty(value = "frilans")
     @Valid
@@ -34,9 +40,11 @@ public class OppgittOpptjeningDto {
         // Json deserialisering
     }
 
-    public OppgittOpptjeningDto(@Valid OppgittFrilansDto frilans,
-                                @Valid List<OppgittEgenNæringDto> egenNæring,
-                                @Valid List<OppgittArbeidsforholdDto> oppgittArbeidsforhold) {
+    public OppgittOpptjeningDto(JournalpostId journalpostId,
+                                OppgittFrilansDto frilans,
+                                List<OppgittEgenNæringDto> egenNæring,
+                                List<OppgittArbeidsforholdDto> oppgittArbeidsforhold) {
+        this.journalpostId = journalpostId;
         this.frilans = frilans;
         this.egenNæring = egenNæring;
         this.oppgittArbeidsforhold = oppgittArbeidsforhold;
@@ -52,5 +60,9 @@ public class OppgittOpptjeningDto {
 
     public List<OppgittArbeidsforholdDto> getOppgittArbeidsforhold() {
         return oppgittArbeidsforhold;
+    }
+
+    public JournalpostId getJournalpostId() {
+        return journalpostId;
     }
 }
