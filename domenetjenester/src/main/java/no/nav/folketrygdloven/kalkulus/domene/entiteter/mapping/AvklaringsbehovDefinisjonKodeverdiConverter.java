051b117770f678e0e953c836b04af0cd337fac36
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-12-13 14:11:36 +0100

    Revert "Revert "Revert "Starter å sende nye koder"""
    This reverts commit a88bbe457f85e1dd0eac06fe6856f1eef9ec6643.

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/domene/entiteter/mapping/AvklaringsbehovDefinisjonKodeverdiConverter.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/domene/entiteter/mapping/AvklaringsbehovDefinisjonKodeverdiConverter.java
@@ -16,6 +16,9 @@ public class AvklaringsbehovDefinisjonKodeverdiConverter implements AttributeCon
         if (dbData == null) {
             return null;
         }
+        if (AvklaringsbehovDefinisjon.KODER.containsKey(dbData)) {
+            return AvklaringsbehovDefinisjon.fraKode(dbData);
+        }
         return  AvklaringsbehovDefinisjon.fraKode(dbData);
     }
 }
