28e633e7404fa253bb6bde3a0929eb0ffd5ec8d7
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-02-16 12:32:52 +0100

    Fikser nullsjekk

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/guitjenester/BeregningsgrunnlagDtoTjeneste.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/guitjenester/BeregningsgrunnlagDtoTjeneste.java
@@ -85,7 +85,7 @@ public class BeregningsgrunnlagDtoTjeneste {
     }
 
     private static void mapForlengelsePerioder(BeregningsgrunnlagGUIInput input, BeregningsgrunnlagDto dto) {
-        if (dto.getForlengelseperioder() != null) {
+        if (input.getForlengelseperioder() != null) {
             dto.setForlengelseperioder(input.getForlengelseperioder().stream().map(p -> new Periode(p.getFomDato(), p.getTomDato())).toList());
         }
     }
