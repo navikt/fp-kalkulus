c9e18ca9fa8e1aeda960b5097cc8a5d7d3f634ab
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-03-22 11:28:36 +0100

    Toggler på gradering mot inntekt og endrer fom-dato til 1. mai

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/tilkommetInntekt/TilkommetInntektPeriodeTjeneste.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/tilkommetInntekt/TilkommetInntektPeriodeTjeneste.java
@@ -43,7 +43,7 @@ public class TilkommetInntektPeriodeTjeneste {
     /**
      * Det er bestemt at gradering mot inntekt ved tilkommet aktivitet skal gjelde perioder fom 1.4.2023.
      */
-    public static final LocalDate FOM_DATO_GRADERING_MOT_INNTEKT = LocalDate.of(2023, 4, 1);
+    public static final LocalDate FOM_DATO_GRADERING_MOT_INNTEKT = LocalDate.of(2023, 5, 1);
 
     public BeregningsgrunnlagDto splittPerioderVedTilkommetInntekt(BeregningsgrunnlagInput input, BeregningsgrunnlagDto beregningsgrunnlag) {
 
