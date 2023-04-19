a71e00e0b9b75d7a7ead8821c77e8da8de16746c
Espen Velsvik <espenjv@users.noreply.github.com>
2023-04-19 10:10:03 +0200

    Fjerner fordeling uten krav logikk som aldri har vore skrudd på i produksjon. Må gå opp denne på nytt i lys av tilkommet inntekt (#4007)

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/ytelse/FordelBeregningsgrunnlagTjenesteUtbGrad.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/ytelse/FordelBeregningsgrunnlagTjenesteUtbGrad.java
@@ -8,7 +8,6 @@ import no.nav.folketrygdloven.kalkulator.input.BeregningsgrunnlagInput;
 import no.nav.folketrygdloven.kalkulator.output.BeregningsgrunnlagRegelResultat;
 import no.nav.folketrygdloven.kalkulator.steg.fordeling.FordelBeregningsgrunnlagTjeneste;
 import no.nav.folketrygdloven.kalkulator.steg.fordeling.omfordeling.OmfordelBeregningsgrunnlagTjeneste;
-import no.nav.folketrygdloven.kalkulator.steg.fordeling.omfordeling.OmfordelingUtenRefusjonskravTjeneste;
 import no.nav.folketrygdloven.kalkulator.steg.fordeling.tilkommetInntekt.TilkommetInntektPeriodeTjeneste;
 import no.nav.folketrygdloven.kalkulus.kodeverk.FagsakYtelseType;
 
@@ -32,17 +31,12 @@ public class FordelBeregningsgrunnlagTjenesteUtbGrad implements FordelBeregnings
     public FordelBeregningsgrunnlagTjenesteUtbGrad(OmfordelBeregningsgrunnlagTjeneste omfordelTjeneste) {
         this.omfordelTjeneste = omfordelTjeneste;
         this.graderingMotInntektEnabled = KonfigurasjonVerdi.get("GRADERING_MOT_INNTEKT", false);
-        this.fordelingUtenKravEnabled = KonfigurasjonVerdi.get("FORDELING_UTEN_KRAV", false);
 
     }
 
     @Override
     public BeregningsgrunnlagRegelResultat omfordelBeregningsgrunnlag(BeregningsgrunnlagInput input) {
         var resultatFraOmfordeling = omfordelTjeneste.omfordel(input, input.getBeregningsgrunnlag());
-        if (fordelingUtenKravEnabled) {
-            resultatFraOmfordeling = new BeregningsgrunnlagRegelResultat(OmfordelingUtenRefusjonskravTjeneste.omfordel(resultatFraOmfordeling.getBeregningsgrunnlag(), input.getYtelsespesifiktGrunnlag()),
-                    resultatFraOmfordeling.getRegelsporinger().orElse(null));
-        }
         if (!graderingMotInntektEnabled) {
             return new BeregningsgrunnlagRegelResultat(resultatFraOmfordeling.getBeregningsgrunnlag(),
                     resultatFraOmfordeling.getRegelsporinger().orElse(null));
