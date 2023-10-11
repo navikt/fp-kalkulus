cf7699c2d29636510468d04ab8ba750cb69ba3b4
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-10-11 10:41:15 +0200

    Logge splitting pga aktivitetsgrad i dev for feilsøk (#4289)

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/periodisering/PeriodiserForAktivitetsgradTjeneste.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/fordeling/periodisering/PeriodiserForAktivitetsgradTjeneste.java
@@ -5,21 +5,37 @@ import static no.nav.folketrygdloven.kalkulus.kodeverk.PeriodeÅrsak.ENDRING_I_A
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.folketrygdloven.kalkulator.felles.periodesplitting.PeriodeSplitter;
 import no.nav.folketrygdloven.kalkulator.felles.periodesplitting.SplittPeriodeConfig;
 import no.nav.folketrygdloven.kalkulator.felles.periodesplitting.StandardPeriodeSplittCombinators;
 import no.nav.folketrygdloven.kalkulator.input.UtbetalingsgradGrunnlag;
 import no.nav.folketrygdloven.kalkulator.input.YtelsespesifiktGrunnlag;
 import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagDto;
+import no.nav.folketrygdloven.kalkulator.modell.svp.PeriodeMedUtbetalingsgradDto;
+import no.nav.folketrygdloven.kalkulator.modell.svp.UtbetalingsgradPrAktivitetDto;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.StandardCombinators;
+import no.nav.k9.felles.konfigurasjon.env.Environment;
 
 public class PeriodiserForAktivitetsgradTjeneste {
+
+    private static final Logger logger = LoggerFactory.getLogger(PeriodiserForAktivitetsgradTjeneste.class);
 
     public static BeregningsgrunnlagDto splittVedEndringIAktivitetsgrad(BeregningsgrunnlagDto beregningsgrunnlag, YtelsespesifiktGrunnlag ytelsespesifiktGrunnlag) {
 
         if (ytelsespesifiktGrunnlag instanceof UtbetalingsgradGrunnlag utbetalingsgradGrunnlag) {
+
+            for (UtbetalingsgradPrAktivitetDto ut : utbetalingsgradGrunnlag.getUtbetalingsgradPrAktivitet()) {
+                for (PeriodeMedUtbetalingsgradDto p : ut.getPeriodeMedUtbetalingsgrad()) {
+                    if (Environment.current().isDev()) {
+                        logger.info("Grunnlagsdata for [{}-{}] aktivitetsgrad {}", p.getPeriode().getFomDato(), p.getPeriode().getTomDato(), p.getAktivitetsgrad().orElse(null));
+                    }
+                }
+            }
 
             var tidslinjePåTversAvArbeidsforhold = utbetalingsgradGrunnlag.getUtbetalingsgradPrAktivitet().stream()
                     .map(a -> {
@@ -33,10 +49,32 @@ public class PeriodiserForAktivitetsgradTjeneste {
                     })
                     .reduce((t1, t2) -> t1.crossJoin(t2, StandardCombinators::coalesceLeftHandSide))
                     .orElse(LocalDateTimeline.empty());
-            return getPeriodeSplitter(beregningsgrunnlag.getSkjæringstidspunkt()).splittPerioder(beregningsgrunnlag, tidslinjePåTversAvArbeidsforhold);
+            if (Environment.current().isDev()) {
+                logger.info("Beregningsgrunnlag før splitting pga tilkommet {}", prettyPrint(beregningsgrunnlag));
+                logger.info("På tvers av arbeidsforhold-tidslinje for stp {}: {}", beregningsgrunnlag.getSkjæringstidspunkt(), prettyPrint(tidslinjePåTversAvArbeidsforhold));
+            }
+            beregningsgrunnlag.getBeregningsgrunnlagPerioder().stream().map(bg -> bg.getPeriode().getFomDato() + "," + bg.getPeriode().getTomDato()).reduce("", (a, b) -> a + ", " + b);
+            BeregningsgrunnlagDto etterSplitt = getPeriodeSplitter(beregningsgrunnlag.getSkjæringstidspunkt()).splittPerioder(beregningsgrunnlag, tidslinjePåTversAvArbeidsforhold);
+            if (Environment.current().isDev()) {
+                logger.info("Beregningsgrunnlag etter splitting pga tilkommet {}", prettyPrint(etterSplitt));
+            }
+            return etterSplitt;
         }
         return beregningsgrunnlag;
     }
+
+    private static String prettyPrint(LocalDateTimeline<BigDecimal> tidslinje) {
+        return tidslinje.stream()
+                .map(s -> "[" + s.getLocalDateInterval().getFomDato() + "," + s.getLocalDateInterval().getTomDato() + "]:" + s.getValue().toPlainString())
+                .reduce("", (a, b) -> a + ", " + b);
+    }
+
+    private static String prettyPrint(BeregningsgrunnlagDto beregningsgrunnlag) {
+        return beregningsgrunnlag.getBeregningsgrunnlagPerioder().stream()
+                .map(bg -> "[" + bg.getPeriode().getFomDato() + "," + bg.getPeriode().getTomDato() + "]")
+                .reduce("", (a, b) -> a + ", " + b);
+    }
+
 
     private static PeriodeSplitter<BigDecimal> getPeriodeSplitter(LocalDate stp) {
         SplittPeriodeConfig<BigDecimal> splittPeriodeConfig = new SplittPeriodeConfig<>(
