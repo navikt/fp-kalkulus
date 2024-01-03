4ebb066710b83186829a46b4d047801d6cd9312f
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-01-03 10:43:08 +0100

    Revert "Kodeverk som string - begynn med serialisering (#3824)" (#4494)
    This reverts commit 40c12ba3bb01f46043bbbdb656dd65cb3c81ed4c.

--- web/src/main/java/no/nav/folketrygdloven/kalkulus/app/jackson/JacksonJsonConfig.java
+++ web/src/main/java/no/nav/folketrygdloven/kalkulus/app/jackson/JacksonJsonConfig.java
@@ -1,7 +1,10 @@
 package no.nav.folketrygdloven.kalkulus.app.jackson;
 
+import java.util.Collection;
+
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -10,11 +13,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
-import no.nav.folketrygdloven.kalkulus.mappers.KalkulusKodeverdiSerializer;
 
 @Provider
 public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
 
+    private static final SimpleModule SER_DESER = createModule();
     private final ObjectMapper objectMapper;
 
     public JacksonJsonConfig() {
@@ -23,15 +26,24 @@ public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.registerModule(createModule());
+        objectMapper.registerModule(SER_DESER);
 
+        Collection<Class<?>> restClasses = new RestImplementationClasses().getImplementationClasses();
 
     }
 
+    public static Module defaultModule() {
+        return SER_DESER;
+    }
+
     private static SimpleModule createModule() {
-        SimpleModule module = new SimpleModule("FT_KALKULUS", new Version(1, 0, 0, null, null, null));
-        module.addSerializer(new KalkulusKodeverdiSerializer());
+        SimpleModule module = new SimpleModule("VL-REST", new Version(1, 0, 0, null, null, null));
+
         return module;
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
     }
 
     @Override
