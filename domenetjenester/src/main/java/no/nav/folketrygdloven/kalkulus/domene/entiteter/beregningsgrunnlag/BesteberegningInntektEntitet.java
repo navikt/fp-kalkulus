e200cff0d0fb37c90e84f8208fd7212f36b0ed53
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-04-15 12:15:22 +0200

    Legger inn entiteter for besteberegning. (#36)

---
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/domene/entiteter/beregningsgrunnlag/BesteberegningInntektEntitet.java
@@ -0,0 +1,142 @@
+package no.nav.folketrygdloven.kalkulus.domene.entiteter.beregningsgrunnlag;
+
+import java.util.Objects;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+import no.nav.folketrygdloven.kalkulus.domene.entiteter.del_entiteter.Arbeidsgiver;
+import no.nav.folketrygdloven.kalkulus.domene.entiteter.del_entiteter.Beløp;
+import no.nav.folketrygdloven.kalkulus.domene.entiteter.del_entiteter.InternArbeidsforholdRef;
+import no.nav.folketrygdloven.kalkulus.felles.jpa.BaseEntitet;
+import no.nav.folketrygdloven.kalkulus.kodeverk.OpptjeningAktivitetType;
+
+
+@Entity(name = "BesteberegningInntektEntitet")
+@Table(name = "BG_BESTEBEREGNING_INNTEKT")
+public class BesteberegningInntektEntitet extends BaseEntitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_BESTEBEREGNING_INNTEKT")
+    private Long id;
+
+    @Version
+    @Column(name = "versjon", nullable = false)
+    private long versjon;
+
+    @Column(name = "opptjening_aktivitet_type", nullable = false)
+    private OpptjeningAktivitetType opptjeningAktivitetType;
+
+    @Embedded
+    private Arbeidsgiver arbeidsgiver;
+
+    @Embedded
+    private InternArbeidsforholdRef arbeidsforholdRef;
+
+    @Embedded
+    @AttributeOverrides(@AttributeOverride(name = "verdi", column = @Column(name = "inntekt")))
+    private Beløp inntekt;
+
+    @OneToOne
+    @JoinColumn(name = "besteberegning_maaned_id", updatable = false, unique = true)
+    private BesteberegningMånedsgrunnlagEntitet besteberegningMåned;
+
+
+    public BesteberegningInntektEntitet(BesteberegningInntektEntitet besteberegningInntektEntitet) {
+        this.arbeidsforholdRef = besteberegningInntektEntitet.arbeidsforholdRef;
+        this.arbeidsgiver = besteberegningInntektEntitet.arbeidsgiver;
+        this.opptjeningAktivitetType = besteberegningInntektEntitet.opptjeningAktivitetType;
+        this.inntekt = besteberegningInntektEntitet.inntekt;
+    }
+
+    public BesteberegningInntektEntitet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public long getVersjon() {
+        return versjon;
+    }
+
+    public OpptjeningAktivitetType getOpptjeningAktivitetType() {
+        return opptjeningAktivitetType;
+    }
+
+    public Arbeidsgiver getArbeidsgiver() {
+        return arbeidsgiver;
+    }
+
+    public InternArbeidsforholdRef getArbeidsforholdRef() {
+        return arbeidsforholdRef == null ? InternArbeidsforholdRef.nullRef() : arbeidsforholdRef;
+    }
+
+    public Beløp getInntekt() {
+        return inntekt;
+    }
+
+    void setBesteberegningMåned(BesteberegningMånedsgrunnlagEntitet besteberegningMåned) {
+        this.besteberegningMåned = besteberegningMåned;
+    }
+
+    public static Builder ny() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final BesteberegningInntektEntitet kladd;
+
+        public Builder() {
+            kladd = new BesteberegningInntektEntitet();
+        }
+
+        public Builder(BesteberegningInntektEntitet besteberegningInntektEntitet, boolean erOppdatering) {
+            if (Objects.nonNull(besteberegningInntektEntitet.getId())) {
+                throw new IllegalArgumentException("Kan ikke bygge på et lagret grunnlag");
+            }
+            if (erOppdatering) {
+                kladd = besteberegningInntektEntitet;
+            } else {
+                kladd = new BesteberegningInntektEntitet(besteberegningInntektEntitet);
+            }
+        }
+
+        public Builder medOpptjeningAktivitetType(OpptjeningAktivitetType opptjeningAktivitetType) {
+            kladd.opptjeningAktivitetType = opptjeningAktivitetType;
+            return this;
+        }
+
+        public Builder medArbeidsgiver(Arbeidsgiver arbeidsgiver) {
+            kladd.arbeidsgiver = arbeidsgiver;
+            return this;
+        }
+
+        public Builder medArbeidsforholdRef(InternArbeidsforholdRef arbeidsforholdRef) {
+            kladd.arbeidsforholdRef = arbeidsforholdRef;
+            return this;
+        }
+
+        public Builder medInntekt(Beløp inntekt) {
+            kladd.inntekt = inntekt;
+            return this;
+        }
+
+        public BesteberegningInntektEntitet build() {
+            Objects.requireNonNull(kladd.opptjeningAktivitetType);
+            Objects.requireNonNull(kladd.inntekt);
+            return kladd;
+        }
+    }
+
+}
