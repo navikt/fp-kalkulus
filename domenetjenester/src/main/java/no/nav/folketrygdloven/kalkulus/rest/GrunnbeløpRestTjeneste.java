3c47dbb8af74f8b00587b9c65a54167668aeadfe
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-04-28 23:34:09 +0200

    Konverter klasser med AbacDto

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/GrunnbeløpRestTjeneste.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/rest/GrunnbeløpRestTjeneste.java
@@ -1,17 +1,16 @@
 package no.nav.folketrygdloven.kalkulus.rest;
 
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -19,29 +18,23 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import no.nav.folketrygdloven.kalkulus.domene.entiteter.BeregningSats;
 import no.nav.folketrygdloven.kalkulus.domene.entiteter.del_entiteter.KoblingReferanse;
-import no.nav.folketrygdloven.kalkulus.felles.v1.Saksnummer;
 import no.nav.folketrygdloven.kalkulus.forvaltning.GrunnbeløpreguleringTjeneste;
 import no.nav.folketrygdloven.kalkulus.kodeverk.GrunnbeløpReguleringStatus;
 import no.nav.folketrygdloven.kalkulus.mapTilKontrakt.MapBeregningSats;
 import no.nav.folketrygdloven.kalkulus.request.v1.HentGrunnbeløpRequest;
 import no.nav.folketrygdloven.kalkulus.request.v1.KontrollerGrunnbeløpRequest;
 import no.nav.folketrygdloven.kalkulus.response.v1.GrunnbeløpReguleringRespons;
+import no.nav.folketrygdloven.kalkulus.sikkerhet.AbacAttributtEmptySupplier;
 import no.nav.folketrygdloven.kalkulus.tjeneste.beregningsgrunnlag.BeregningsgrunnlagRepository;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
-import no.nav.vedtak.sikkerhet.abac.AbacDto;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.vedtak.sikkerhet.abac.StandardAbacAttributtType;
+import no.nav.vedtak.sikkerhet.abac.TilpassetAbacAttributt;
 import no.nav.vedtak.sikkerhet.abac.beskyttet.ActionType;
 import no.nav.vedtak.sikkerhet.abac.beskyttet.ResourceType;
 
@@ -71,7 +64,7 @@ public class GrunnbeløpRestTjeneste {
     @Operation(description = "Hent grunnbeløp for angitt dato", summary = ("Returnerer grunnbeløp for dato."), tags = "grunnbelop")
     @BeskyttetRessurs(actionType = ActionType.READ, resourceType = ResourceType.FAGSAK, sporingslogg = false)
     @Path("/grunnbelop")
-    public Response hentGrunnbeløp(@NotNull @Valid HentGrunnbeløpRequestAbacDto spesifikasjon) {
+    public Response hentGrunnbeløp(@TilpassetAbacAttributt(supplierClass = AbacAttributtEmptySupplier.class) @NotNull @Valid HentGrunnbeløpRequest spesifikasjon) {
         BeregningSats grunnbeløp = beregningsgrunnlagRepository.finnGrunnbeløp(spesifikasjon.getDato());
         final Response response = Response.ok(MapBeregningSats.map(grunnbeløp)).build();
         return response;
@@ -83,7 +76,7 @@ public class GrunnbeløpRestTjeneste {
             summary = ("Returnerer en liste over koblinger og status for gregulering for hver kobling."), tags = "grunnbelop")
     @BeskyttetRessurs(actionType = ActionType.READ, resourceType = ResourceType.FAGSAK, sporingslogg = false)
     @Path("/kontrollerGregulering")
-    public Response hentGrunnbeløp(@NotNull @Valid KontrollerGrunnbeløpRequestAbacDto spesifikasjon) {
+    public Response hentGrunnbeløp(@TilpassetAbacAttributt(supplierClass = KontrollerGrunnbeløpRequestAbacSupplier.class) @NotNull @Valid KontrollerGrunnbeløpRequest spesifikasjon) {
         List<UUID> referanser = spesifikasjon.getKoblinger();
         Map<UUID, GrunnbeløpReguleringStatus> resultat = new HashMap<>();
 
@@ -97,42 +90,12 @@ public class GrunnbeløpRestTjeneste {
         return Response.ok(respons).build();
     }
 
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonInclude(value = JsonInclude.Include.NON_ABSENT, content = JsonInclude.Include.NON_EMPTY)
-    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
-    public static class HentGrunnbeløpRequestAbacDto extends HentGrunnbeløpRequest implements AbacDto {
-
-
-        @JsonCreator
-        public HentGrunnbeløpRequestAbacDto(@JsonProperty(value = "dato", required = true) @Valid @NotNull LocalDate dato) {
-            super(dato);
-        }
-
+    public static class KontrollerGrunnbeløpRequestAbacSupplier implements Function<Object, AbacDataAttributter> {
         @Override
-        public AbacDataAttributter abacAttributter() {
-            final var abacDataAttributter = AbacDataAttributter.opprett();
-            return abacDataAttributter;
-        }
-    }
-
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonInclude(value = JsonInclude.Include.NON_ABSENT, content = JsonInclude.Include.NON_EMPTY)
-    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
-    public static class KontrollerGrunnbeløpRequestAbacDto extends KontrollerGrunnbeløpRequest implements AbacDto {
-
-
-        @JsonCreator
-        public KontrollerGrunnbeløpRequestAbacDto(@JsonProperty(value = "koblinger", required = true) @Valid @NotNull @Size(min = 1) List<UUID> koblinger,
-                                                  @JsonProperty(value = "saksnummer", required = true) @NotNull @Valid Saksnummer saksnummer) {
-            super(koblinger, saksnummer);
-        }
-
-        @Override
-        public AbacDataAttributter abacAttributter() {
-            final var abacDataAttributter = AbacDataAttributter.opprett().leggTil(StandardAbacAttributtType.SAKSNUMMER, getSaksnummer());
-            return abacDataAttributter;
+        public AbacDataAttributter apply(Object o) {
+            var req = (KontrollerGrunnbeløpRequest) o;
+            return AbacDataAttributter.opprett()
+                .leggTil(StandardAbacAttributtType.SAKSNUMMER, req.getSaksnummer().verdi());
         }
     }
 
