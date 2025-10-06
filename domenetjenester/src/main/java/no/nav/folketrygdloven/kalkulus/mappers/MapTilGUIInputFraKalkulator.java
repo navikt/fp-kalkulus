d4bb68aab18d85a0e065996f0a1737124f7f4607
remhol <193506135+remhol@users.noreply.github.com>
2025-10-06 15:44:48 +0200

    TFP-6405: Lager toggle for flytting av ap for refusjonsfrist

--- domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/mappers/MapTilGUIInputFraKalkulator.java
+++ domenetjenester/src/main/java/no/nav/folketrygdloven/kalkulus/mappers/MapTilGUIInputFraKalkulator.java
@@ -9,6 +9,7 @@ import no.nav.folketrygdloven.kalkulus.domene.entiteter.beregningsgrunnlag.Bereg
 import no.nav.folketrygdloven.kalkulus.domene.entiteter.kobling.KoblingEntitet;
 import no.nav.folketrygdloven.kalkulus.felles.v1.KalkulatorInputDto;
 import no.nav.folketrygdloven.kalkulus.typer.AktørId;
+import no.nav.foreldrepenger.konfig.Environment;
 
 public class MapTilGUIInputFraKalkulator {
 
@@ -17,21 +18,21 @@ public class MapTilGUIInputFraKalkulator {
                                                                    KalkulatorInputDto input,
                                                                    BeregningsgrunnlagGrunnlagEntitet beregningsgrunnlagGrunnlagEntitet) {
         var skjæringstidspunkt = Skjæringstidspunkt.builder()
-                .medFørsteUttaksdato(input.getSkjæringstidspunkt())
-                .medSkjæringstidspunktOpptjening(input.getSkjæringstidspunkt()).build();
-        var ref = KoblingReferanse.fra(
-                kobling.getYtelseType(),
-                new AktørId(kobling.getAktørId().getId()),
-                kobling.getId(),
-                kobling.getKoblingReferanse().getReferanse(),
-                Optional.empty(),
-                skjæringstidspunkt);
+            .medFørsteUttaksdato(input.getSkjæringstidspunkt())
+            .medSkjæringstidspunktOpptjening(input.getSkjæringstidspunkt())
+            .build();
+        var ref = KoblingReferanse.fra(kobling.getYtelseType(), new AktørId(kobling.getAktørId().getId()), kobling.getId(),
+            kobling.getKoblingReferanse().getReferanse(), Optional.empty(), skjæringstidspunkt);
         var iayGrunnlagMappet = MapIAYTilKalulator.mapGrunnlag(input.getIayGrunnlag());
-        return new BeregningsgrunnlagGUIInput(
-                ref,
-                iayGrunnlagMappet,
-                MapFraKalkulator.mapKravperioder(input.getRefusjonskravPrArbeidsforhold(), input.getRefusjonskravDatoer(), input.getIayGrunnlag(), input.getSkjæringstidspunkt()),
-                MapFraKalkulator.mapYtelsespesifiktGrunnlag(kobling.getYtelseType(), input, Optional.ofNullable(beregningsgrunnlagGrunnlagEntitet)));
+        var beregningsgrunnlagGuiInput = new BeregningsgrunnlagGUIInput(ref, iayGrunnlagMappet,
+            MapFraKalkulator.mapKravperioder(input.getRefusjonskravPrArbeidsforhold(), input.getRefusjonskravDatoer(), input.getIayGrunnlag(),
+                input.getSkjæringstidspunkt()),
+            MapFraKalkulator.mapYtelsespesifiktGrunnlag(kobling.getYtelseType(), input, Optional.ofNullable(beregningsgrunnlagGrunnlagEntitet)));
+        beregningsgrunnlagGuiInput.leggTilToggle("refusjonsfrist.flytting", erIkkeProd());
+        return beregningsgrunnlagGuiInput;
     }
 
+    private static boolean erIkkeProd() {
+        return !Environment.current().isProd();
+    }
 }
